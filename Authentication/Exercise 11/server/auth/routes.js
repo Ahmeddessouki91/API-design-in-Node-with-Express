19491ff8d401ae33184fc6e591125e5418a8b63f
Glen Pinner <glen.pinner@gmail.com>
2017-02-22 14:24:22 +0000

    Exercise 10 - Authentication (JSON web tokens).

---
+++ Authentication/Exercise 11/server/auth/routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const controller = require('./controller');
+const verifyUser = require('./auth').verifyUser;
+
+// before we send back a jwt, lets check
+// the password and username match what is in the DB
+router.post('/signin', verifyUser(), controller.signin);
+
+module.exports = router;
