44fcc56f0698a8144b8972b732f97fe135a8d6f2
Glen Pinner <glen.pinner@gmail.com>
2017-03-01 10:28:42 +0000

    Exercise 12 - Securing routes.

---
+++ Securing Routes/Exercise 12/index.js
@@ -0,0 +1,15 @@
+// intro point for our server.
+// PRO-TIP: if you have an index.js file
+// on the root of a folder in node
+// you can just require that folder and node will
+// automatically require the index.js on the root
+
+// setup config first before anything by requiring it
+const config = require('./server/config/config');
+const app = require('./server/server');
+const logger = require('./server/util/logger');
+
+app.listen(config.port);
+logger.log('listening on http://localhost:' + config.port);
+
+
